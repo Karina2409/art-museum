b1bc550681d076cdb423515e2ad50df2a24d39fc
Karina2409 <karina.siardziuk@mail.ru>
2025-03-30 16:21:21 +0300

    feat: create favourite-page component, add routing to the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from '@pages/home-page/home-page.component';
+import { FavouritePageComponent } from '@pages/favourite-page/favourite-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'favourite',
+    title: 'Favourite',
+    component: FavouritePageComponent,
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+];
