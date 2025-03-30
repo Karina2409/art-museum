6f0a2f510a3aff94c65ee944a42c1204136b71a7
Karina2409 <karina.siardziuk@mail.ru>
2025-03-30 16:52:17 +0300

    feat: add home menu display condition

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Component, computed, inject, signal } from '@angular/core';
+import { NgIf, NgOptimizedImage } from '@angular/common';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [NgOptimizedImage, RouterLink],
+  imports: [NgOptimizedImage, RouterLink, NgIf],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  public isHomePage = computed(() => this.currentUrl() === '/home');
+
+  private router = inject(Router);
+  private currentUrl = signal(this.router.url);
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentUrl.set(event.urlAfterRedirects);
+      }
+    });
+  }
+}
