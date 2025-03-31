4d148e1f1f7e578770ed5151420443828896d606
Karina2409 <karina.siardziuk@mail.ru>
2025-03-31 11:19:13 +0300

    feat: create picture components and picture html template

---
+++ src/app/components/picture/picture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-picture',
+  imports: [],
+  templateUrl: './picture.component.html',
+  standalone: true,
+  styleUrl: './picture.component.scss',
+})
+export class PictureComponent {}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { PictureComponent } from '@components/picture/picture.component';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
+  standalone: true,
+  imports: [PictureComponent],
 })
 export class HomePageComponent {}
